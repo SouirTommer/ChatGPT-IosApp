b83c51fc6ff6d3ba8519e6706f3a8482f0ed6029
Tommer <souirtommer@gmail.com>
2023-03-08 13:07:04 +0800

    Initial Commit

---
+++ ChatGPT/ChatGPTApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatGPTApp.swift
+//  ChatGPT
+//
+//  Created by ST SE on 8/3/2023.
+//
+
+import SwiftUI
+
+@main
+struct ChatGPTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

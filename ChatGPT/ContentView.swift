b5cf0a88c3c983546652d782fc3b85028a31d4c9
Tommer <souirtommer@gmail.com>
2023-03-08 14:15:48 +0800

    modify commit

--- ChatGPT/ContentView.swift
+++ ChatGPT/ContentView.swift
@@ -10,7 +10,7 @@ import WebKit
 
 struct ContentView: View {
     
-    // 初始化webView实例
+    // init webView
     var webView: WKWebView = {
         let config = WKWebViewConfiguration()
         let websiteDataStore = WKWebsiteDataStore.default()
@@ -19,38 +19,37 @@ struct ContentView: View {
         return webView
     }()
     
-    // 初始化State存储cookies和lastURL信息
+    
     @State private var cookies: [HTTPCookie] = []
     @State private var lastURL: URL? = nil
     
     var body: some View {
         WebView(webView: webView, cookies: $cookies, lastURL: $lastURL)
             .onAppear(perform: {
-                // 如果有上次浏览的URL，恢复到上次浏览位置
+                // view last url
                 if let lastURL = lastURL {
                     let request = URLRequest(url: lastURL)
                     webView.load(request)
                 } else {
-                    // 否则打开默认网站
+                    // view default web
                     let url = URL(string: "https://chat.openai.com")!
                     let request = URLRequest(url: url)
                     webView.load(request)
                 }
             })
             .onChange(of: cookies, perform: { _ in
-                // 当cookies更新时保存到websiteDataStore中
+                // update cookie
                 webView.configuration.websiteDataStore.httpCookieStore.getAllCookies { cookies in
                     self.cookies = cookies
                 }
             })
             .onChange(of: webView.url, perform: { url in
-                // 当浏览的URL更新时保存到lastURL中
+                // url save to lasturl
                 lastURL = url
             })
     }
 }
 
-// 为了简化 ContentView 中的代码，这里定义了一个 WebView 视图
 struct WebView: UIViewRepresentable {
     
     let webView: WKWebView
@@ -63,7 +62,6 @@ struct WebView: UIViewRepresentable {
     
     func updateUIView(_ uiView: WKWebView, context: Context) {}
     
-    // 确保所有的网页数据保存在 websiteDataStore 中
     static func dismantleUIView(_ uiView: Self.UIViewType, coordinator: ()) {
         uiView.configuration.websiteDataStore.httpCookieStore.getAllCookies { cookies in
             uiView.configuration.websiteDataStore.fetchDataRecords(ofTypes: WKWebsiteDataStore.allWebsiteDataTypes()) { records in
